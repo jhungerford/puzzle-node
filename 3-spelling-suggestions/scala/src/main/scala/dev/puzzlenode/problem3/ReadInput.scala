784afc89092c55d43d3e2156cc7dcd8a48e08098
John Hungerford <jhungerford@tendrilinc.com>
2015-11-15 06:32:21 -0700

    Input reader.

--- 3-spelling-suggestions/scala/src/main/scala/dev/puzzlenode/problem3/ReadInput.scala
+++ 3-spelling-suggestions/scala/src/main/scala/dev/puzzlenode/problem3/ReadInput.scala
@@ -4,5 +4,20 @@ import scala.io.Source
 import scala.util.Try
 
 object ReadInput {
-  def apply(in: Source): Try[List[WordCase]] = ???
+  def apply(in: Source): Try[List[WordCase]] = Try {
+    val lines = in.getLines()
+
+    val numLines = lines.next().toInt
+    lines.next()
+
+    (1 to numLines).map { caseNum =>
+      val wordCase = WordCase(lines.next(), lines.next(), lines.next())
+
+      if (caseNum < numLines) {
+        lines.next()
+      }
+
+      wordCase
+    }.toList
+  }
 }
