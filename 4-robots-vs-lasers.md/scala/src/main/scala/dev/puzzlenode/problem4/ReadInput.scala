98708d36523c5ae21f89b260a8df478e9b8548fb
John Hungerford <jhungerford@tendrilinc.com>
2015-11-16 08:25:37 -0700

    Input reader.

--- 4-robots-vs-lasers.md/scala/src/main/scala/dev/puzzlenode/problem4/ReadInput.scala
+++ 4-robots-vs-lasers.md/scala/src/main/scala/dev/puzzlenode/problem4/ReadInput.scala
@@ -4,5 +4,12 @@ import scala.io.Source
 import scala.util.Try
 
 object ReadInput {
-  def apply(source: Source): Try[List[FactoryFloor]] = ???
+  def apply(source: Source): Try[List[FactoryFloor]] = Try {
+    source.getLines().foldLeft( (0, List.empty[FactoryFloor]) ) { case ((i, floors), line) => i match {
+      case 0 => (1, floors :+ FactoryFloor(Lasers(line), null, -1))
+      case 1 => (2, floors.init :+ floors.last.copy(initialIndex = line.indexOf('X')))
+      case 2 => (3, floors.init :+ floors.last.copy(southLasers = Lasers(line)))
+      case 3 => (0, floors)
+    }}._2
+  }
 }
